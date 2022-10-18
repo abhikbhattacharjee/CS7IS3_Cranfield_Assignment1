cad73563b4f7ff2383ce41f01538b029c9fb1d70
Abhik Bhattacharjee <abhikbhattacharjee.mit@gmail.com>
2022-10-18 19:57:55 +0100

    Update Query.java

--- src/main/java/org/example/Query.java
+++ src/main/java/org/example/Query.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.core.SimpleAnalyzer;
 import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
@@ -13,13 +14,14 @@ import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.*;
 import org.apache.lucene.search.similarities.BM25Similarity;
-import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.similarities.BooleanSimilarity;
 import org.apache.lucene.search.similarities.MultiSimilarity;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.QueryBuilder;
+import sun.print.PrinterJobWrapper;
 
 import java.io.File;
 import java.io.IOException;
@@ -34,12 +36,10 @@ import java.util.Scanner;
 public class Query {
     private static String INDEX_PATH = "index/";
     private static String OUTPUT_FILE = "output/results.txt";
-    private static int NUM_RESULTS = 10;
+    private static int NUM_RESULTS = 200;
 
     public static void searcher(String[] args) throws Exception {
-        //System.out.println("Hello world!");
         String filePath = "../cran.qry";
-//        String text = Files.readString(Paths.get(filePath));
         String text = Utils.readFullFile(filePath);
         int index = -1;
         int count = 0;
@@ -49,7 +49,8 @@ public class Query {
 
         IndexSearcher indexSearcher = new IndexSearcher(indexReader);
 
-        System.out.println("Please select the type of Similarity:\n 1. BM25 for BM25Similarity()\n 2. Boolean for BooleanSimilarity()\n"
+        System.out.println("Please select the type of Similarity:\n 1. BM25 for BM25Similarity()\n " +
+                "2. Boolean for BooleanSimilarity()\n"
         );
         Scanner myObj1 = new Scanner(System.in);
         String similarityChoice = myObj1.nextLine();
@@ -57,46 +58,48 @@ public class Query {
         switch (similarityChoice) {
             case "BM25":
                 indexSearcher.setSimilarity(new BM25Similarity());
+                System.out.println("Selected Similarity is: BM25Similarity()");
                 break;
             case "Boolean":
                 indexSearcher.setSimilarity(new BooleanSimilarity());
+                System.out.println("Selected Similarity is: BooleanSimilarity()");
                 break;
             default:
                 indexSearcher.setSimilarity(new BM25Similarity());
+                System.out.println("Selected Default Similarity : BM25Similarity()");
         }
 
-        System.out.println("Selected Similarity is: " + similarityChoice +"Similarity()");
 
-        System.out.println("Please select the type of Analyzer for Query Parser:\n 1. Standard for StandardAnalyzer()\n 2. Simple for SimpleAnalyzer()\n 3. Whitespace for WhitespaceAnalyzer()\n"
+        System.out.println("Please select the type of Analyzer for Query Parser:\n 1. Standard for StandardAnalyzer()\n" +
+                " 2. Simple for SimpleAnalyzer()\n 3. Whitespace for WhitespaceAnalyzer()\n"
         );
         Scanner myObj = new Scanner(System.in);
         String analyzerChoice = myObj.nextLine();
         Analyzer analyzer = null;
 
         switch (analyzerChoice) {
-            case "Standard":
+            case "Standard": {
                 analyzer = new StandardAnalyzer();
+                System.out.println("Selected Analyzer for Query Parser is: StandardAnalyzer()");
                 break;
+            }
             case "Simple":
                 analyzer = new SimpleAnalyzer();
+                System.out.println("Selected Analyzer for Query Parser is: SimpleAnalyzer()");
                 break;
             case "Whitespace":
                 analyzer = new WhitespaceAnalyzer();
+                System.out.println("Selected Analyzer for Query Parser is: WhitespaceAnalyzer()");
                 break;
-            default:
+            default: {
+                System.out.println("Selected Default Analyzer for Query Parser : StandardAnalyzer()");
                 analyzer = new StandardAnalyzer();
+            }
         }
-
-        System.out.println("Selected Analyzer for Query Parser is: " + analyzerChoice +"Analyzer()");
 
         File resultFile = new File(OUTPUT_FILE);
         resultFile.getParentFile().mkdirs();
         PrintWriter writer = new PrintWriter(resultFile, StandardCharsets.UTF_8.name());
-
-        HashMap<String, Float> fieldMap = new HashMap<>();
-        fieldMap.put("title", 10f);
-        fieldMap.put("author", 2f);
-        fieldMap.put("words", 10f);
 
         ArrayList<Integer> indexData = new ArrayList<Integer>();
 
@@ -108,7 +111,6 @@ public class Query {
             if (index != -1) {
                 indexData.add(index);
             }
-            //System.out.println("Index: " + index);
             count++;
         }
         while (index != -1);
@@ -116,42 +118,42 @@ public class Query {
             indexData.add(text.length());
         }
 
-        //System.out.println("Count: " + indexData.size());
-
         String word_doc = null;
-        for (int i = 1; i < indexData.size(); i++) {
+        HashMap<String, Float> boostMap = new HashMap<>();
+        boostMap.put("title", 100f);
+        boostMap.put("author", 0.1f);
+        boostMap.put("biblio", 0.1f);
+        boostMap.put("words", 10000f);
+
+        MultiFieldQueryParser queryParser = new MultiFieldQueryParser(
+                new String[]{"title", "author", "biblio", "words"}, analyzer, boostMap);
+        int i;
+        int j;
+        for (i = 1; i < indexData.size(); i++) {
             String midText = text.substring(indexData.get(i - 1), indexData.get(i));
-            //System.out.println(midText.substring(midText.indexOf(".A") + ".T\n".length(), midText.indexOf(".B")));
             String index_doc = midText.substring(midText.indexOf(".I") + ".I\n".length(), midText.indexOf(".W"));
+            index_doc = index_doc.trim();
             word_doc = midText.substring(midText.indexOf(".W") + ".W\n".length(), midText.length());
-            //System.out.println(word_doc);
-            //System.out.println(i);
+            word_doc = word_doc.trim();
 
-            MultiFieldQueryParser MFQqueryParser = new MultiFieldQueryParser(
-                    new String[]{"title", "author", "bibliograph", "text"}, analyzer, fieldMap);
+            String queryString = queryParser.escape(word_doc);
+            org.apache.lucene.search.Query query = queryParser.parse(queryString);
 
-            String queryString = QueryParser.escape(word_doc.trim());
-            org.apache.lucene.search.Query query = MFQqueryParser.parse(queryString);
-            //System.out.println(" >> " + index_doc);
-            ScoreDoc[] sd = indexSearcher.search(query, NUM_RESULTS).scoreDocs;
-            for (int j = 0; j < sd.length; j++) {
-                Document hd = indexSearcher.doc(sd[j].doc);
-                String line = (index_doc.trim().replaceAll("\n","")) + " 0 " +
-                        (hd.get("docid").replaceAll("\n","")) + " 0 " + sd[j].score + " STANDARD";
-                writer.println(line);
-                /*
-                 * index_doc (it has a return carriage and new line feed character)
-                 *  0 hd.get("docid") (which also has a new line feed character)
-                 */
+            TopDocs topDocs = indexSearcher.search(query, NUM_RESULTS);
+            int numTotalHits = Math.toIntExact(topDocs.totalHits.value);
+            topDocs = indexSearcher.search(query, numTotalHits);
+            ScoreDoc [] hits = topDocs.scoreDocs;
+
+            for (j = 0; j < hits.length; j++) {
+                Document hitDocument = indexSearcher.doc(hits[j].doc);
+                writer.println(i + " 0 " + hitDocument.get("docid")
+                         + " " + (j+1) + " " + hits[j].score + " STANDARD");
             }
 
         }
-
         indexReader.close();
         writer.close();
         directory.close();
         System.out.println("Output: " + OUTPUT_FILE);
-
-
     }
 }
